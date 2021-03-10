892b53c55699030ebacbc490c960113cfedc3857
s.igarashi <50ra4.dev.dev@gmail.com>
2021-03-11 00:58:00 +0900

    add: npm scripts "generate-env"

---
+++ scripts/utils.ts
@@ -0,0 +1,38 @@
+import * as fs from 'fs';
+import { Either, tryCatch, toError } from 'fp-ts/Either';
+
+type ObjectType = Record<string, unknown>;
+type EmptyObject = Record<string, never>;
+
+export const readJsonFile = <T extends ObjectType = EmptyObject>(path: string): Either<Error, T> =>
+  tryCatch(
+    () => JSON.parse(fs.readFileSync(path, 'utf-8')) as T,
+    (e) => toError(e),
+  );
+
+export const createJsonFile = <T extends ObjectType = EmptyObject>(path: string, data: T): Either<Error, void> =>
+  tryCatch(
+    () => fs.writeFileSync(path, JSON.stringify(data)),
+    (e) => toError(e),
+  );
+
+export const createEnvironmentFile = <T extends ObjectType = EmptyObject>(path: string, data: T): Either<Error, void> =>
+  tryCatch(
+    () =>
+      fs.writeFileSync(
+        path,
+        Object.entries(data)
+          .map(([key, value]) => `${key} = ${value}`)
+          .join('\n'),
+      ),
+    (e) => toError(e),
+  );
+
+export const throwError = <E = unknown>(error: E): never => {
+  throw error;
+};
+
+export const exitProcessError = <E = unknown>(error: E): never => {
+  console.error(error);
+  process.exit(1);
+};
